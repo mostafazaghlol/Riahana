ae66e9808308506534c12af78caf750f995ddfb4
mostafa <mostafas.zaghlol@gmail.com>
2018-02-07 14:39:38 +0200

    finish signup design

--- app/src/main/java/com/mostafa/android/riahana/Login.java
+++ app/src/main/java/com/mostafa/android/riahana/Login.java
@@ -1,18 +1,31 @@
 package com.mostafa.android.riahana;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
 
 public class Login extends AppCompatActivity {
     ImageView imageViewAdds;
+    TextView textViewCreateOne;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         imageViewAdds = (ImageView)findViewById(R.id.imageAdds);
+        textViewCreateOne = (TextView)findViewById(R.id.createOne);
+        final Intent i = new Intent(this,Signup.class);
+
+        textViewCreateOne.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            startActivity(i);
+            }
+        });
         Picasso.with(this).load(R.drawable.adds).into(imageViewAdds);
     }
 }
---
+++ app/src/main/java/com/mostafa/android/riahana/Signup.java
@@ -0,0 +1,20 @@
+package com.mostafa.android.riahana;
+
+import android.provider.ContactsContract;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class Signup extends AppCompatActivity {
+    ImageView imageViewBack;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+
+    public void backicon(View view) {
+        onBackPressed();
+    }
+}
